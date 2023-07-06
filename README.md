234560f50a7e104338e623ac39340aca0e25a32a
nikitandro <nik.vish.93@mail.ru>
2023-07-06 17:40:30 +0500

    added project description in README.md
    also added main tag in sign up page

---
+++ README.md
@@ -0,0 +1,22 @@
+# Мессенджер
+
+Учебный проект по курсу "Мидл-фронтенд разработчик" от Яндекс.Практикума.
+
+Для начала нужно выполнить команду `npm install`, а после `npm run start`, чтобы запустить проект.
+
+Команды:
+- Запуск проекта: `npm run start`
+- Запуск проекта в режиме разработки: `npm run dev`
+- Билд: `npm run build`
+
+[__Прототип мессенджера__](https://www.figma.com/file/p5gFCNuVfzzSKmsuMtIpW1/Practicum-Messenger?node-id=12%3A44&mode=dev)
+
+Страницы:
+- [Авторизация](https://deploy--illustrious-biscuit-2e7513.netlify.app/sign-in)
+- [Регистрация](https://deploy--illustrious-biscuit-2e7513.netlify.app/sign-up)
+- [Чаты](https://deploy--illustrious-biscuit-2e7513.netlify.app/chats)
+- [Профиль](https://deploy--illustrious-biscuit-2e7513.netlify.app/profile)
+- [Редактирование профиля](https://deploy--illustrious-biscuit-2e7513.netlify.app/profile-edit)
+- [Редактирование пароля](https://deploy--illustrious-biscuit-2e7513.netlify.app/profile-edit-password)
+- [500](https://deploy--illustrious-biscuit-2e7513.netlify.app/500)
+- [404](https://deploy--illustrious-biscuit-2e7513.netlify.app/asdasd)
--- src/pages/sign-up-page/sign-up-page.tmpl.ts
+++ src/pages/sign-up-page/sign-up-page.tmpl.ts
@@ -5,21 +5,23 @@ import link from '../../components/link';
 
 // language=handlebars
 export default `
-    <div class="sign-in">
-        <form action="" class="sign-in-form">
-            <h1 class="sign-in-form__title">Регистрация</h1>
-            <div class="sign-in-form__inputs">
-                ${input('email', 'Почта', 'email')}
-                ${input('text', 'Логин', 'login')}
-                ${input('text', 'Имя', 'first_name')}
-                ${input('text', 'Фамилия', 'second_name')}
-                ${input('phone', 'Телефон', 'phone')}
-                ${input('password', 'Пароль', 'password')}
-                ${input('password', 'Пароль (ещё раз)', 'password')}
-            </div>
-            <div class="sign-in-form__buttons">
-                ${button('Зарегистрироваться')}
-                ${link('Войти', '/sign-in')}
-            </div>
-        </form>
-    </div>`;
+    <main>
+        <div class="sign-in">
+            <form action="" class="sign-in-form">
+                <h1 class="sign-in-form__title">Регистрация</h1>
+                <div class="sign-in-form__inputs">
+                    ${input('email', 'Почта', 'email')}
+                    ${input('text', 'Логин', 'login')}
+                    ${input('text', 'Имя', 'first_name')}
+                    ${input('text', 'Фамилия', 'second_name')}
+                    ${input('phone', 'Телефон', 'phone')}
+                    ${input('password', 'Пароль', 'password')}
+                    ${input('password', 'Пароль (ещё раз)', 'password')}
+                </div>
+                <div class="sign-in-form__buttons">
+                    ${button('Зарегистрироваться')}
+                    ${link('Войти', '/sign-in')}
+                </div>
+            </form>
+        </div>
+    </main>`;

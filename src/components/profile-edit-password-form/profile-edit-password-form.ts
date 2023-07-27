43a845731d34744ca923415b6955f6fe3e8f7122
nikitandro <nik.vish.93@mail.ru>
2023-07-27 22:28:16 +0500

    added small fixes

--- src/components/profile-edit-password-form/profile-edit-password-form.ts
+++ src/components/profile-edit-password-form/profile-edit-password-form.ts
@@ -3,20 +3,13 @@ import Button from '../button';
 import Form from '../form';
 import Input from '../input';
 import profileEditPasswordFormTmpl from './profile-edit-password-form.tmpl';
-const profile = {
-    first_name: 'Никита',
-    email: 'nik.vish.93@mail.ru',
-    login: 'nikitandro',
-    phone: '+79193813732',
-    second_name: 'Вишняков',
-    display_name: 'Никита',
-};
 
 export default class ProfileEditPasswordForm extends Form {
     constructor() {
         const inputClass = 'profile-list-item__value profile-list-item__input';
         super(profileEditPasswordFormTmpl, {
             props: {
+                first_name: 'Никита',
                 avatarInput: new AvatarInput(),
                 saveButton: new Button({ props: { text: 'Сохранить' } }),
                 oldPasswordInput: new Input({

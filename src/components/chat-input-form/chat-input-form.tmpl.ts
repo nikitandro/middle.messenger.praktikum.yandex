6bc856eb3854fae793fca1605f86c7f54310808b
nikitandro <nik.vish.93@mail.ru>
2023-07-28 12:59:14 +0500

    have done work on chat input form

--- src/components/chat-input-form/chat-input-form.tmpl.ts
+++ src/components/chat-input-form/chat-input-form.tmpl.ts
@@ -1,9 +1,5 @@
-import clipIcon from '../../assets/icons/clip-icon.svg';
-import arrowInCircleRight from '../../assets/icons/arrow-in-circle-right.svg';
-
 export default /*hbs*/ `
-    <button class="chat-input-form__clip-button"><img src="${clipIcon}" alt=""/></button>
-    {{!-- <input class="chat-input-form__input" placeholder="Сообщение" name="message"> --}}
+    {{{clipButton}}}
     {{{input}}}
-    <button class="chat-input-form__send-button"><img src="${arrowInCircleRight}" alt=""></button>
+    {{{sendButton}}}
 `;
--- src/components/chat-input-form/chat-input-form.ts
+++ src/components/chat-input-form/chat-input-form.ts
@@ -2,6 +2,7 @@ import Form from '../form';
 import Input from '../input';
 import chatInputFormTmpl from './chat-input-form.tmpl';
 import './chat-input-form.scss';
+import Button from '../button';
 
 export default class ChatInputForm extends Form {
     constructor() {
@@ -12,6 +13,15 @@ export default class ChatInputForm extends Form {
                         class: 'chat-input-form__input',
                         placeholder: 'Сообщение',
                         name: 'message',
+                    },
+                }),
+                clipButton: new Button({
+                    attrs: { class: 'chat-input-form__clip-button', type: 'button' },
+                }),
+                sendButton: new Button({
+                    attrs: {
+                        type: 'submit',
+                        class: 'chat-input-form__send-button',
                     },
                 }),
             },

c5f990eb191962438c231a604cd5380a0b289141
nikitandro <nik.vish.93@mail.ru>
2023-08-21 12:40:38 +0500

    added small fixes

--- src/components/chat-list-item/chat-list-item.tmpl.ts
+++ src/components/chat-list-item/chat-list-item.tmpl.ts
@@ -3,7 +3,12 @@ export default /*hbs*/ `
     <div class="chat-image"></div>
     <div class="chat-list-item__texts">
         <h2 class="chat-list-item__name">{{title}}</h2>
-        <p class="chat-list-item__content">{{last_message.content}}</p>
+        {{#if last_message}}
+        <div class="chat-list-item__preview">
+        <span class="chat-list-item__display-name">{{last_message.user.display_name}}:</span>
+        <span class="chat-list-item__content">{{last_message.content}}</span>
+        </div>
+        {{/if}}
     </div>
 </div>
 <div class="chat-list-item__right-part">
